1b4e12e50bcf65c47048d67bb7075a04593b7333
And <strawer@mail.ru>
2025-04-23 19:40:09 +0300

    hw-20 task1-done

--- src/ui/tests/demo-login-form/register.spec.ts
+++ src/ui/tests/demo-login-form/register.spec.ts
@@ -1,276 +1,164 @@
-// import test, { expect } from "@playwright/test";
+import test, { expect } from "@playwright/test";
 
-// test.describe("[UI] [demo-login-form] Registration", () => {
-//   const validCredentials = {
-//     username: "ValidUser",
-//     password: "Password123!",
-//   };
+test.describe("[UI] [demo-login-form] Registration", () => {
+  interface IRegistrationTestData {
+    testName: string;
+    username: string;
+    password: string;
+    message: string;
+  }
 
-//   const validCredentialsForNegativeTests = {
-//     username: "ValidUser2",
-//     password: "Password123!2",
-//   };
+  const registrationValidTestData: IRegistrationTestData[] = [
+    {
+      testName: "Successfully registered with max valid data",
+      username: "aB!11111111111111111111",
+      password: "123AAbb!111111111111",
+      message:
+        "Successfully registered! Please, click Back to return on login page",
+    },
+    {
+      testName: "Successfully registered with min valid data",
+      username: "aB!",
+      password: "123AAbb!",
+      message:
+        "Successfully registered! Please, click Back to return on login page",
+    },
+  ];
 
-//   const validCredentialsMinLength = {
-//     username: "Usr",
-//     password: "Passw0rd",
-//   };
+  const registrationInvalidTestData = [
+    {
+      testName: "Should not register with more than 40 characters in username",
+      username: "ThisIsAnInvalidUsernameWithMoreThan40Characters",
+      password: "Password123!",
+      message: "Username must be between 3 and 40 characters",
+    },
+    {
+      testName: "Should not register with empty Username",
+      username: "",
+      password: "Password123!",
+      message: "Username is required",
+    },
+    {
+      testName: "Should not register with empty Password",
+      username: "ValidUser",
+      password: "",
+      message: "Password is required",
+    },
+    {
+      testName:
+        "Should not register with valid Username containing only spaces",
+      username: "   ",
+      password: "Password123!",
+      message: "Prefix and postfix spaces are not allowed",
+    },
+    {
+      testName:
+        "Should not register with valid Username containing prefix space",
+      username: " ValidUser",
+      password: "Password123!",
+      message: "Prefix and postfix spaces are not allowed",
+    },
+    {
+      testName:
+        "Should not register with valid Username containing postfix space",
+      username: "ValidUser ",
+      password: "Password123!",
+      message: "Prefix and postfix spaces are not allowed",
+    },
+    {
+      testName:
+        "Should not register with valid Username shorter than the minimum length",
+      username: "Us",
+      password: "Password123!",
+      message: "Username should contain at least 3 characters",
+    },
+    {
+      testName:
+        "Should not register with Password shorter than the minimum length",
+      username: "ValidUser",
+      password: "Passw0",
+      message: "Password should contain at least 8 characters",
+    },
+    {
+      testName:
+        "Should not register with Password longer than the maximum length",
+      username: "ValidUser",
+      password: "ThisIsAnInvalidPasswordWithMoreThan20Characters",
+      message: "Password must be between 8 and 20 characters",
+    },
+    {
+      testName: "Should not register with Password without uppercase letter",
+      username: "ValidUser",
+      password: "password123",
+      message: "Password should contain at least one character in upper case",
+    },
+    {
+      testName: "Should not register with Password without lowercase letter",
+      username: "ValidUser",
+      password: "PASSWORD123",
+      message: "Password should contain at least one character in lower case",
+    },
+    {
+      testName: "Should not register with  Password containing only spaces",
+      username: "ValidUser",
+      password: "     ",
+      message: "Password is required",
+    },
+  ];
 
-//   const validCredentialsMaxLength = {
-//     username: "ThisIsAValidUsernameWith40Characters",
-//     password: "ThisIsAValidPasswordWith20CharacteR1",
-//   };
+  const errorMessageLocator = "#errorMessageOnRegister";
 
-//   const invalidCredentials = {
-//     shortUsername: "Us",
-//     longUsername: "ThisIsAnInvalidUsernameWithMoreThan40Characters",
-//     shortPassword: "Passw0",
-//     longPassword: "ThisIsAnInvalidPasswordWithMoreThan20Characters",
-//     noUppercasePassword: "password123",
-//     noLowercasePassword: "PASSWORD123",
-//     onlySpacesPassword: "     ",
-//   };
+  test.beforeEach(async ({ page }) => {
+    await page.goto("https://anatoly-karpovich.github.io/demo-login-form/");
+    await page.locator("#registerOnLogin").click();
+  });
 
-//   const errorMessageLocator = "#errorMessageOnRegister";
+  test.describe("Positive Cases", () => {
+    registrationValidTestData.forEach(
+      ({ testName, username, password, message }) => {
+        test.only(testName, async ({ page }) => {
+          await page.goto(
+            "https://anatoly-karpovich.github.io/demo-login-form/"
+          );
+          await page.locator("#registerOnLogin").click();
+          const form = page.locator(".registerForm");
+          await form.locator("#userNameOnRegister").fill(username);
+          await form.locator("#passwordOnRegister").fill(password);
+          await form.locator("#register").click();
+          await expect(form.locator("#errorMessageOnRegister")).toHaveText(
+            message
+          );
+          const localStorageData = await page.evaluate((username: string) => {
+            return window.localStorage.getItem(username);
+          }, username);
+          expect(localStorageData).toBeTruthy();
+          const user = JSON.parse(localStorageData!);
+          expect(user).toMatchObject({
+            name: username,
+            password,
+          });
+        });
+      }
+    );
+  });
 
-//   test.beforeEach(async ({ page }) => {
-//     await page.goto("https://anatoly-karpovich.github.io/demo-login-form/");
-//     await page.locator("#registerOnLogin").click();
-//   });
-
-//   test.describe("Positive Cases", () => {
-//     test("Should register with valid credentials", async ({ page }) => {
-//       await page.locator("#userNameOnRegister").fill(validCredentials.username);
-//       await page.locator("#passwordOnRegister").fill(validCredentials.password);
-//       await page.locator("#register").click();
-
-//       //assert
-//       const notification = page.locator(errorMessageLocator);
-//       await expect(notification).toContainText("Successfully registered!");
-//     });
-
-//     test("Should register with valid MIN length credentials", async ({
-//       page,
-//     }) => {
-//       await page
-//         .locator("#userNameOnRegister")
-//         .fill(validCredentialsMinLength.username);
-//       await page
-//         .locator("#passwordOnRegister")
-//         .fill(validCredentialsMinLength.password);
-//       await page.locator("#register").click();
-
-//       //assert
-//       const notification = page.locator(errorMessageLocator);
-//       await expect(notification).toContainText("Successfully registered!");
-//     });
-
-//     test("Should register with valid MAX length credentials", async ({
-//       page,
-//     }) => {
-//       await page
-//         .locator("#userNameOnRegister")
-//         .fill(validCredentialsMaxLength.username);
-//       await page
-//         .locator("#passwordOnRegister")
-//         .fill(validCredentialsMaxLength.password);
-//       await page.locator("#register").click();
-
-//       //assert
-//       const notification = page.locator(errorMessageLocator);
-//       await expect(notification).toContainText("Successfully registered!");
-//     });
-//   });
-
-//   test.describe("Negative Cases", () => {
-//     test("Should not register with empty Username", async ({ page }) => {
-//       await page
-//         .locator("#passwordOnRegister")
-//         .fill(validCredentialsMaxLength.password);
-//       await page.locator("#register").click();
-
-//       //assert
-//       const notification = page.locator(errorMessageLocator);
-//       await expect(notification).toContainText("Username is required");
-//     });
-
-//     test("Should not register with empty Password", async ({ page }) => {
-//       await page
-//         .locator("#userNameOnRegister")
-//         .fill(validCredentialsMaxLength.username);
-//       await page.locator("#register").click();
-
-//       //assert
-//       const notification = page.locator(errorMessageLocator);
-//       await expect(notification).toContainText("Password is required");
-//     });
-
-//     test("Should not register with valid Username containing only spaces", async ({
-//       page,
-//     }) => {
-//       await page
-//         .locator("#userNameOnRegister")
-//         .fill(invalidCredentials.onlySpacesPassword);
-//       await page.locator("#passwordOnRegister").fill(validCredentials.password);
-//       await page.locator("#register").click();
-
-//       //assert
-//       const notification = page.locator(errorMessageLocator);
-//       await expect(notification).toContainText(
-//         "Prefix and postfix spaces are not allowed"
-//       );
-//     });
-
-//     test("Should not register with valid Username containing prefix space", async ({
-//       page,
-//     }) => {
-//       await page
-//         .locator("#userNameOnRegister")
-//         .fill(" " + validCredentialsForNegativeTests.username);
-//       await page.locator("#passwordOnRegister").fill(validCredentials.password);
-//       await page.locator("#register").click();
-
-//       //assert
-//       const notification = page.locator(errorMessageLocator);
-//       await expect(notification).toContainText(
-//         "Prefix and postfix spaces are not allowed"
-//       );
-//     });
-
-//     test("Should not register with valid Username containing postfix space", async ({
-//       page,
-//     }) => {
-//       await page
-//         .locator("#userNameOnRegister")
-//         .fill(validCredentialsForNegativeTests.username + " ");
-//       await page.locator("#passwordOnRegister").fill(validCredentials.password);
-//       await page.locator("#register").click();
-
-//       //assert
-//       const notification = page.locator(errorMessageLocator);
-//       await expect(notification).toContainText(
-//         "Prefix and postfix spaces are not allowed"
-//       );
-//     });
-
-//     test("Should not register with valid Username shorter than the minimum length", async ({
-//       page,
-//     }) => {
-//       await page
-//         .locator("#userNameOnRegister")
-//         .fill(invalidCredentials.shortUsername);
-//       await page.locator("#passwordOnRegister").fill(validCredentials.password);
-//       await page.locator("#register").click();
-
-//       //assert
-//       const notification = page.locator(errorMessageLocator);
-//       await expect(notification).toContainText(
-//         "Username should contain at least 3 characters"
-//       );
-//     });
-
-//     test("Should not register with valid  Username longer than the maximum length", async ({
-//       page,
-//     }) => {
-//       await page
-//         .locator("#userNameOnRegister")
-//         .fill(invalidCredentials.longUsername);
-//       await page.locator("#passwordOnRegister").fill(validCredentials.password);
-//       await page.locator("#register").click();
-
-//       //assert
-//       const notification = page.locator(errorMessageLocator);
-//       await expect(notification).toContainText(
-//         "Username must be between 3 and 40 characters"
-//       );
-//     });
-
-//     test("Should not register with Password shorter than the minimum length", async ({
-//       page,
-//     }) => {
-//       await page
-//         .locator("#userNameOnRegister")
-//         .fill(validCredentialsForNegativeTests.username);
-//       await page
-//         .locator("#passwordOnRegister")
-//         .fill(invalidCredentials.shortPassword);
-//       await page.locator("#register").click();
-
-//       //assert
-//       const notification = page.locator(errorMessageLocator);
-//       await expect(notification).toContainText(
-//         "Password should contain at least 8 characters"
-//       );
-//     });
-
-//     test("Should not register with Password longer than the maximum length", async ({
-//       page,
-//     }) => {
-//       await page
-//         .locator("#userNameOnRegister")
-//         .fill(validCredentialsForNegativeTests.username);
-//       await page
-//         .locator("#passwordOnRegister")
-//         .fill(invalidCredentials.longPassword);
-//       await page.locator("#register").click();
-
-//       //assert
-//       const notification = page.locator(errorMessageLocator);
-//       await expect(notification).toContainText(
-//         "Password must be between 8 and 20 characters"
-//       );
-//     });
-
-//     test("Should not register with Password without uppercase letter", async ({
-//       page,
-//     }) => {
-//       await page
-//         .locator("#userNameOnRegister")
-//         .fill(validCredentialsForNegativeTests.username);
-//       await page
-//         .locator("#passwordOnRegister")
-//         .fill(invalidCredentials.noUppercasePassword);
-//       await page.locator("#register").click();
-
-//       //assert
-//       const notification = page.locator(errorMessageLocator);
-//       await expect(notification).toContainText(
-//         "Password should contain at least one character in upper case"
-//       );
-//     });
-
-//     test("Should not register with Password without lowercase letter", async ({
-//       page,
-//     }) => {
-//       await page
-//         .locator("#userNameOnRegister")
-//         .fill(validCredentialsForNegativeTests.username);
-//       await page
-//         .locator("#passwordOnRegister")
-//         .fill(invalidCredentials.noLowercasePassword);
-//       await page.locator("#register").click();
-
-//       //assert
-//       const notification = page.locator(errorMessageLocator);
-//       await expect(notification).toContainText(
-//         "Password should contain at least one character in lower case"
-//       );
-//     });
-
-//     test("Should not register with  Password containing only spaces", async ({
-//       page,
-//     }) => {
-//       await page
-//         .locator("#userNameOnRegister")
-//         .fill(validCredentialsForNegativeTests.username);
-//       await page
-//         .locator("#passwordOnRegister")
-//         .fill(invalidCredentials.onlySpacesPassword);
-//       await page.locator("#register").click();
-
-//       //assert
-//       const notification = page.locator(errorMessageLocator);
-//       await expect(notification).toContainText("Password is required");
-//     });
-//   });
-// });
+  test.describe("Negative Cases", () => {
+    registrationInvalidTestData.forEach(
+      ({ testName, username, password, message }) => {
+        test.only(testName, async ({ page }) => {
+          await page.goto(
+            "https://anatoly-karpovich.github.io/demo-login-form/"
+          );
+          await page.locator("#registerOnLogin").click();
+          const form = page.locator(".registerForm");
+          await form.locator("#userNameOnRegister").fill(username);
+          await form.locator("#passwordOnRegister").fill(password);
+          await form.locator("#register").click();
+          await expect(form.locator("#errorMessageOnRegister")).toHaveText(
+            message
+          );
+        });
+      }
+    );
+  });
+});
